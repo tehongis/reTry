c1f97e686bb0708b1f2ecbf279bda544c22f01fa
Tero Hongisto <tero.hongisto@kolumbus.fi>
2018-06-16 11:52:10 +0300

    updated makefile for windows and linux use.

---
+++ A500Dev/compile.cmd
@@ -0,0 +1,1 @@
+vasmm68k_mot_win32.exe -devpac -Fhunkexe -o "C:\Users\Public\Documents\Amiga Files\WinUAE\Harddrives\DevHDD\demo.exe" source.s
--- A500Dev/source.s
+++ A500Dev/source.s
@@ -2,6 +2,7 @@
 	include LVOs.i
 	include Hards.i
 
+	section maincode,code
 
 main
 	move.l	#CUSTOM,a6
@@ -9,11 +10,11 @@ main
 	move.w  #%0000000001111111,DMACON(a6)
 	move.w  #%1100000000000000,INTENA(a6)
 	move.w  #%0011111111111111,INTENA(a6)
-	
+
 	lea		myCopper,a0
 	move.l	a0,COP1LCH(a6)
-	
-.mainloop	
+
+.mainloop
 	bra .mainloop
 
 irq
@@ -28,22 +29,24 @@ OLD_INTREQ
 OLD_ADKCON
 	dc.l	0
 
+		section chipdata,data,chip
+
 myCopper
-	dc.w  	$3200,BPLCON0
+	dc.w  $3200,BPLCON0
 	dc.w	$0000,BPLCON1
 	dc.w	$0050,BPL1MOD
 	dc.w	$0050,BPL2MOD
-	dc.w	$0000,BPL1PTH	
+	dc.w	$0000,BPL1PTH
 	dc.w	$0000,BPL1PTL
-	dc.w	$0000,BPL2PTH	
+	dc.w	$0000,BPL2PTH
 	dc.w	$0000,BPL2PTL
-	dc.w	$0000,BPL3PTH	
+	dc.w	$0000,BPL3PTH
 	dc.w	$0000,BPL3PTL
-	
+
 	dc.w	$2c81,DIWSTRT
 	dc.w	$c8d1,DIWSTOP
 	dc.w	$0038,DDFSTRT
-	dc.w	$00d0,DDFSTOP   
+	dc.w	$00d0,DDFSTOP
 	dc.w	$0180,$0000
 	dc.w	$0182,$0fff
 	dc.w	$ffff,$fffe
