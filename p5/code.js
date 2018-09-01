0e298265e0ed5f77db5a59f4c8e643693f1de1db
Tero Hongisto <tero.hongisto@kolumbus.fi>
2018-09-01 22:59:40 +0300

    test

--- p5/code.js
+++ p5/code.js
@@ -45,9 +45,9 @@ function draw() {
   imageMode(CENTER);
 
   x_location = x + (sin(counter1) * (width/2));
-  y_location = y + (sin(counter2) * (height/2));
+  y_location = y + (sin(counter2) * (height/4));
 
-  image(colors, 0, y_location,width,colors.height);
+  image(colors, x, y_location,width,colors.height*2);
   image(logo, x_location, y);
 
   counter1 += 0.02;
