ddee23ed019d59fbee8213e6f79b8bbb0d28432a
Tero Hongisto <tehongis@gmail.com>
2026-08-26 14:17:38 +0300

    Micro to own repo

--- PC/m68kMicro/newbase/tests/disasm.c
+++
@@ -1,5 +0,0 @@
-#include "disasm.h"
-
-char disasm_buf[256];
-m68k_disassemble(&cpu, cpu.pc, disasm_buf, sizeof(disasm_buf));
-printf("PC 0x%08X: %s\n", cpu.pc, disasm_buf);   
--- PC/m68kMicro/newbase/tests/interrupt.c
+++
@@ -1,2 +0,0 @@
-// Trigger a keyboard interrupt when character available
-m68k_set_irq(&cpu, 2);  // Level 2 interrupt   
